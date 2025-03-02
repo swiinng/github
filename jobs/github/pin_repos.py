6d763e97a350860213aa504d26ab6172046a7827
Ruchit Patel <ruchittpatell@gmail.com>
2025-03-02 11:51:19 -0800

    sync central repos

---
+++ jobs/github/pin_repos.py
@@ -0,0 +1,3 @@
+# update profile pins based on recent activity or some algo
+
+# make sure all central repos are the only ones pinned
--- jobs/github/sync_central_repo.py
+++ jobs/github/sync_central_repo.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
 from os import getenv
+from string import Template
 
-from github import Github
-from Levenshtein import distance as levenshtein_distance
+from github import Github, UnknownObjectException
+from Levenshtein import ratio as levenshtein_ratio
+
 # type checking
 from github.Organization import Organization
 from github.PaginatedList import PaginatedList
@@ -10,32 +12,91 @@ from github.Repository import Repository
 
 from utils.auth_utils import AuthUtils
 
-# find which one is monorepo (closest levenshtein to org name)
-# create if does not exist
-# set the about/repo description + link
-# create or update readme
-# build a repo cache? lucene or whoosh? and dont update if nothing's changed
+CENTRAL_REPO_DESCRIPTION_TEMPLATE = Template('A central hub for all repositories within `$orgname`')
+
+def generate_markdown(org: Organization, central_repo: Repository, org_repos: PaginatedList[Repository]) -> bytes | str:
+    markdown = f"# {org.name}\n"
+    markdown += f"[{org.html_url}]({org.html_url})\n\n"
+    markdown += f"_{org.description}_\n\n"
+    markdown += "---\n\n"
+
+    for repo in org_repos:
+        if repo.name == '.github' or repo.name == central_repo.name:
+          continue
+        markdown += f"## {repo.name}\n"
+        markdown += f"🔗 [Source Code]({repo.html_url})  |  🌍 [Website]({repo.homepage})\n\n"
+        markdown += f"{repo.description}\n\n"
+
+    return markdown
+
+def update_readme(org: Organization, central_repo: Repository, org_repos: PaginatedList[Repository]) -> None:
+  readme_path = "README.md"
+  content = generate_markdown(org=org, central_repo=central_repo, org_repos=org_repos)
+  commit_message = "Sync Central Repo — Update README.md"
+
+  try:
+    file = central_repo.get_contents(readme_path)
+    central_repo.update_file(
+        path=readme_path,
+        message=commit_message,
+        content=content,
+        sha=file.sha,
+    )
+  except UnknownObjectException:
+    central_repo.create_file(
+        path=readme_path,
+        message=commit_message,
+        content=content,
+    )
+
+def create_central_repo(org: Organization, template_repo: Repository) -> Repository:
+  return org.create_repo_from_template(
+    name=org.name,
+    repo=template_repo,
+    description=CENTRAL_REPO_DESCRIPTION_TEMPLATE.safe_substitute(orgname=org.name),
+    private=False
+  )
 
 def get_central_repo(org: Organization, org_repos: PaginatedList[Repository]) -> Repository:
-  return min(org_repos, key=lambda repo: levenshtein_distance(repo.name, org.name))
+  if not org_repos or not org_repos.totalCount:
+    return None
+  
+  best_match = None
+  best_score = 0
 
-def sync_central_repo(org: Organization, github: Github):
+  # names should match at least x%
+  for repo in org_repos:
+      score = levenshtein_ratio(repo.name, org.name)
+      if score > best_score and score >= 0.7:
+          best_score = score
+          best_match = repo
+
+  return best_match
+  
+def sync_central_repo(org: Organization, template_repo: Repository, github: Github):
   org_repos = github.get_organization(org.login).get_repos()
   central_repo = get_central_repo(org=org, org_repos=org_repos)
+  if not central_repo:
+    central_repo = create_central_repo(org=org, template_repo=template_repo)
 
-  # validate looks like central repo
+  # set or update repo attributes
+  central_repo.edit(
+    description=CENTRAL_REPO_DESCRIPTION_TEMPLATE.safe_substitute(orgname=org.name),
+    homepage=org.html_url
+  )
 
-  print(central_repo)
-  # for repo in org_repos:
-  #   print(repo)
+  update_readme(org=org, central_repo=central_repo, org_repos=org_repos)
 
-def sync_central_repos(github: Github) -> None:
+def sync_central_repos(template_name: str, github: Github) -> None:
+  template_repo = github.get_repo(full_name_or_id=template_name)
+  
   orgs = github.get_user().get_orgs()
   for org in orgs:
     try:
-      sync_central_repo(org=org, github=github)
+      sync_central_repo(org=org, template_repo=template_repo, github=github)
+      print(f"Successfully synced central repo for org `{org.name}`")
     except Exception as e:
-      print(f"Failed to sync central repository for org: {org.login}: ", e)
+      print(f"Failed to sync central repository for org: {org.name}: ", e)
 
 def main():
   if not load_dotenv():
@@ -51,9 +112,14 @@ def main():
   if not token_name:
     print("Failed to get token")
     return
+  
+  template_name = getenv("CENTRAL_REPO_TEMPLATE_NAME")
+  if not template_name:
+    print("Failed to get central repo template name")
+    return
 
   with Github(auth=AuthUtils.with_token(user=botman, name=token_name)) as github:
-    sync_central_repos(github=github)
+    sync_central_repos(template_name=template_name, github=github)
   
 if __name__ == '__main__':
   main()
